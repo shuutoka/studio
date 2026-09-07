ff0328befea4323887ba57445522f86546bad7ad
shuutoka <80158458+shuutoka@users.noreply.github.com>
2026-09-08 00:29:50 +0200

    Update favicon and icon configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,23 @@ export const metadata: Metadata = {
     statusBarStyle: "black-translucent",
   },
   icons: {
-    icon: "./favicon.svg",
-    shortcut: "./favicon.svg",
+  icon: [
+    {
+      url: "./favicon-32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      url: "./icon-192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+  ],
+  shortcut: "./favicon-32.png",
+  apple: {
+    url: "./apple-touch-icon.png",
+    sizes: "180x180",
+    type: "image/png",
   },
 };
 
